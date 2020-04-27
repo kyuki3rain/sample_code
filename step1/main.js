aae959d2781117a3166bb010f79c1ecf46a0fd0a
kyuki3rain <kyuki3rain@gmail.com>
2020-04-27 23:10:24 +0900

    d

---
+++ step1/main.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    document.getElementById("backHome").addEventListener("click", () => {
+        window.location.href = '../index.html';
+    });
+    document.getElementById("submit").addEventListener("click", () => {
+        if (document.getElementById("q1ans").value === "富本銭") {
+            window.location.href = '../index.html';
+        } else {
+            window.alert("答えが間違っています");
+        }
+    });
+
+}
