88bf414385b3c1588c2ffec0416665a024876a59
kyuki3rain <kyuki3rain@gmail.com>
2020-05-01 11:19:32 +0900

    e

--- step1/main.js
+++ step1/main.js
@@ -1,13 +1,12 @@
-window.onload = function() {
-    document.getElementById("backHome").addEventListener("click", () => {
-        window.location.href = '../index.html';
-    });
-    document.getElementById("submit").addEventListener("click", () => {
-        if (document.getElementById("q1ans").value === "富本銭") {
-            window.location.href = '../index.html';
-        } else {
-            window.alert("答えが間違っています");
-        }
-    });
-
-}
+window.onload = function () {
+  document.getElementById("backHome").addEventListener("click", () => {
+    window.location.href = "../index.html";
+  });
+  document.getElementById("submit").addEventListener("click", () => {
+    if (document.getElementById("q1ans").value === "富本銭") {
+      window.location.href = "../step2/index.html";
+    } else {
+      window.alert("答えが間違っています");
+    }
+  });
+};
